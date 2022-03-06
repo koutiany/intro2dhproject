4b4f93a4cfa8b7ec25fa34664415bdc66121ac8d
Martin Mayer <97232560+damnty@users.noreply.github.com>
2022-03-06 21:44:32 +0100

    Add files via upload
    updated version with better names for function and values

--- get_lines_all_characters.R
+++ get_lines_all_characters.R
@@ -12,14 +12,14 @@ episode_getter <- function(link) {
     link %>%
     read_html() %>%
     html_nodes(".topictitle") # Get the html node name with 'selector gadget'
-
+  
   episode_links <- title_reference %>%
     html_attr("href") %>%
     gsub("^.", "", .) %>%
     paste0(main_url, .) %>%
     setNames(title_reference %>% html_text()) %>%
     enframe(name = "episode_name", value = "link")
-
+  
   episode_links
 }
 
@@ -50,14 +50,6 @@ all_episodes$text <- map(all_episodes$link, episode_fun)
 # Some episodes (e.g. S08E09 or S04E06) don't have the characters with the dialoge or not the full script. we need to exclud them.
 
 all_episodes$count <- map_dbl(all_episodes$text, nrow)
-#----------------------------------------------------------------
-
-#We can extend the previous tibble to be a bit more organized by separating the episode-season column into separate season and episo numbers.
-
-all_episodes <- all_episodes %>%
-  separate(episode_name, c("season", "episode"), "-", extra = "merge") %>%
-  separate(season, c("season", "episode_number"), sep = "x")
-all_episodes
 
 #Break the lines down per character                                                (ALL CHARACTERS)
 
@@ -67,7 +59,7 @@ lines_all_characters <- map(filter(all_episodes, count > 15) %>% pull(text), ~ {
     mutate(episode_lines_id = 1:nrow(.))
 }) %>%
   setNames(filter(all_episodes, count > 15) %>% # name according to episode
-             unite(season_episode, season, episode_number, sep = "x") %>%
+             unite(season_episode, episode_name, sep = "x") %>%
              pull(season_episode)) %>%
   enframe() %>%
   unnest() %>%
@@ -76,21 +68,20 @@ lines_all_characters <- map(filter(all_episodes, count > 15) %>% pull(text), ~ {
 
 # Seperate Season from Episode
 
-clean_speaker_regex <- "^[0-9]*"
-clean_speaker2_regex <- "x[0-9]*"
+get_season_regex <- "^[0-9]*"
+get_episode_regex <- "x[0-9]*"
 
 lines_all_characters$episode <- lines_all_characters$name
 lines_all_characters <- lines_all_characters[, c(1,5,2,3,4)]
 
-get_season <- str_extract(lines_all_characters$name, clean_speaker_regex)
+get_season <- str_extract(lines_all_characters$name, get_season_regex)
 lines_all_characters$name <- get_season
+names(lines_all_characters)[1] <- 'season'
 
-get_ep_number <- str_extract(lines_all_characters$episode, clean_speaker2_regex)
+get_ep_number <- str_extract(lines_all_characters$episode, get_episode_regex)
 get_ep_number <- str_remove(get_ep_number, "x")
 lines_all_characters$episode <- get_ep_number
 
-#change name of first column to season
-names(lines_all_characters)[1] <- 'season'
 colnames(lines_all_characters)
 
 #add column for the speaker
@@ -101,12 +92,27 @@ lines_all_characters <- lines_all_characters[, c(1,2, 6, 3, 4, 5)]
 regex_speaker = "^[a-zA-Z]*:\\s|^[a-zA-Z]*\\s\\([a-zA-Z\\s]*\\):\\s|^[a-zA-Z'-]*?[\\s-][a-zA-Z0-9'-]*:|^[a-zA-Z']*?\\s[a-zA-Z']*?\\s[a-zA-Z']*:"
 speakers <- str_extract(lines_all_characters$text, regex_speaker)
 speakers <- str_remove(speakers, ":")
+speakers <- gsub("\\s*\\([^\\)]+\\)","",as.character(speakers))
+
 lines_all_characters$speaker <- tolower(speakers)
 
 #clean text
 clear_text <- str_remove(lines_all_characters$text, regex_speaker)
+clear_text <- str_remove(clear_text, '^\\([a-zA-Z\\s]*\\)')
 #clear_text <- str_remove(lines_all_characters$text, '^?.\\([a-zA-Z.\\s]*\\)')
 lines_all_characters$text <- clear_text
 
-#export to csv for next step usage
-write.csv(lines_all_characters,"/Users/TianyiKou/Documents/lines_all.csv", row.names = FALSE)
+#unification of character names based on regular expression patterns
+all_characters <- lines_all_characters %>% group_by(speaker) %>% summarize(text_bound = paste(text, collapse = " "))
+
+lines_all_characters %>% mutate(speaker = if_else(str_detect(speaker,"mar?h?s?h?ah?ll?|marshall's voice|marshall young"), "marshall", speaker))
+lines_all_characters %>% mutate(speaker = if_else(str_detect(speaker,"ted\\s|narrtor"), "ted", speaker))
+lines_all_characters %>% mutate(speaker = if_else(str_detect(speaker,"lill?y\\s"), "lily", speaker))
+lines_all_characters %>% mutate(speaker = if_else(str_detect(speaker,"robii?b?n\\s"), "robin", speaker))
+lines_all_characters %>% mutate(speaker = if_else(str_detect(speaker,"bare?n?en?y"), "barney", speaker))
+
+#colappsing of text based on groups
+new <- lines_all_characters %>% group_by(season, episode, speaker) %>% summarize(text_bound = paste(text, collapse = " "))
+lines_per_characters <- lines_all_characters %>% group_by(speaker = "barney", season, episode) %>% summarize(text_bound = paste(text, collapse = " "))
+
+write.csv(new,"/Users/marma/Documents/studium/Digital Humanities/lines_all_characters.csv", row.names = FALSE)
